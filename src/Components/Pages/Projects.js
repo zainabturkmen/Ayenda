5c0f88556db72f00a7fbe48fe437437c8b612444
zainabturkmen <zainabturkmen7@gmail.com>
2025-01-10 09:24:33 +0430

    commit

--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -2,6 +2,7 @@ import React from "react";
 import backimg from "../../Assets/projects/back.png";
 import dots from "../../Assets/about/dots.png";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
@@ -28,11 +29,93 @@ const Projects = () => {
   return (
     <Wrapper>
       <Hero />
-      <div></div>
+      <div className="container">
+        {/* Content */}
+        <div className="content">
+          <h1>Book a Free Consulting</h1>
+          <p>
+            "Your Growth Journey Starts Here. Schedule a free consultation with
+            our experts to discuss your needs, challenges, and goals. Together,
+            we’ll craft a strategy that delivers results. Click below to book
+            your session today!
+          </p>
+        </div>
+
+        {/* Projects */}
+        <div className="projects"></div>
+
+        {/* Testimonails */}
+        <div className="testimonail"></div>
+
+        {/* content */}
+        <div className="content">
+          <h1>Ready to Elevate Your Brand?</h1>
+          <p>
+            "Your Growth Journey Starts Here. Ayendah is here to help you
+            achieve your goals, whether through digital marketing, creative
+            productions, or business consulting. Let’s build your future
+            together!
+          </p>
+          <Link to="/contact" className="link">
+            Contact us
+          </Link>
+        </div>
+      </div>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  .container {
+    display: flex;
+    flex-direction: column;
+    width: 310px;
+    margin: 0 auto;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    gap: 0.3em;
+    margin: 0 auto;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 21px;
+    font-weight: 400;
+    line-height: 172%; /* 36.12px */
+  }
+
+  p {
+    text-align: center;
+    font-size: 12px;
+    font-weight: 300;
+    line-height: 180%; /* 21.6px */
+  }
+
+  .link {
+    background-color: var(--primary-yellow-color);
+    padding: 0.5em 1em;
+    border-radius: 10px;
+    margin-top: 1em;
+  }
+
+  @media (min-width: 1000px) {
+    .container {
+      width: 1000px;
+    }
+    h1 {
+      font-size: 30px;
+    }
+
+    p {
+      font-size:20px;
+    }
+  }
+`;
 
 export default Projects;
