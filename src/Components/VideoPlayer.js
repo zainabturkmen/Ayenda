de5ea755790c75e7494a2c26eacc412ea64d6d91
zainabturkmen <zainabturkmen7@gmail.com>
2025-04-14 18:36:29 +0430

    Ayenda

--- src/Components/VideoPlayer.js
+++ src/Components/VideoPlayer.js
@@ -6,27 +6,24 @@ import speach from "../Assets/videos/speach.mp4";
 const VideoPlayer = () => {
   return (
     <div className="p-6">
-      <h1 className="text-3xl font-bold mb-6 text-center md:text-4xl">
+      <h1 className="text-3xl font-bold mb-6 text-center md:text-4xl w-[300px] mx-auto">
         Amazing Case Study
       </h1>
       <div className="flex flex-col md:flex-row justify-center items-center gap-10">
-        <div className="flex flex-col items-center cursor-pointer w-[300px] md:w-full ">
-          <h2 className="text-1xl mb-2 md:text-2xl">Cute Cat Video</h2>
-          <ReactPlayer
-            url={catVideo}
-            controls={true}
-            height="350px"
-            width="500px"
-          />
+        <div className="flex flex-col items-center cursor-pointer w-[300px] md:w-[600px]">
+          <h2 className="text-xl mb-2 md:text-2xl">Cute Cat Video</h2>
+          <div className="w-full aspect-video">
+            {" "}
+            {/* This keeps the correct aspect ratio */}
+            <ReactPlayer url={catVideo} controls width="100%" height="350px" />
+          </div>
         </div>
-        <div className="flex flex-col items-center cursor-pointer w-[300px] md:w-full">
-          <h2 className="text-1xl mb-2 md:text-2xl">Inspiring Speech</h2>
-          <ReactPlayer
-            url={speach}
-            controls={true}
-            height="350px"
-            width="500px"
-          />
+
+        <div className="flex flex-col items-center cursor-pointer w-[300px] md:w-[600px]">
+          <h2 className="text-xl mb-2 md:text-2xl">Inspiring Speech</h2>
+          <div className="w-full aspect-video">
+            <ReactPlayer url={speach} controls width="100%" height="350px" />
+          </div>
         </div>
       </div>
     </div>
