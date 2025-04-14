ef93744da92a6af8f80e2d03ec269f7673f6b310
zainabturkmen <zainabturkmen7@gmail.com>
2025-04-14 18:46:16 +0430

    Ayenda

--- src/Components/VideoPlayer.js
+++ src/Components/VideoPlayer.js
@@ -5,12 +5,12 @@ import speach from "../Assets/videos/speach.mp4";
 
 const VideoPlayer = () => {
   return (
-    <div className="p-6">
+    <div className="p-6 w-[1100px] m-auto">
       <h1 className="text-3xl font-bold mb-6 text-center md:text-4xl w-[300px] mx-auto">
         Amazing Case Study
       </h1>
       <div className="flex flex-col md:flex-row justify-center items-center gap-10">
-        <div className="flex flex-col items-center cursor-pointer w-[300px] md:w-[600px]">
+        <div className="flex flex-col items-center cursor-pointer w-[300px] md:w-[700px]">
           <h2 className="text-xl mb-2 md:text-2xl">Cute Cat Video</h2>
           <div className="w-full aspect-video">
             {" "}
@@ -19,7 +19,7 @@ const VideoPlayer = () => {
           </div>
         </div>
 
-        <div className="flex flex-col items-center cursor-pointer w-[300px] md:w-[600px]">
+        <div className="flex flex-col items-center cursor-pointer w-[300px] md:w-[700px]">
           <h2 className="text-xl mb-2 md:text-2xl">Inspiring Speech</h2>
           <div className="w-full aspect-video">
             <ReactPlayer url={speach} controls width="100%" height="350px" />
